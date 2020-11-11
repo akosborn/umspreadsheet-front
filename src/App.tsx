cef80aa1c16caa43cb719963868165805a9db717
Andrew <andrewosborn93@gmail.com>
2020-11-11 00:37:33 -0500

    React init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TopNav from './top-nav/TopNav';
+
+function App() {
+  return (
+    <div className="App">
+      <TopNav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/top-nav/TopNav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TopNav() {
+  return (
+    <div style={{ height: 40, textAlign: 'left', padding: '4px 10px', background: '#024B80' }}>
+      <h2 style={{ color: '#FFF' }}>RateUM</h2>
+    </div>
+  );
+}
+
+export default TopNav;
